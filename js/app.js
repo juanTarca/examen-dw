a910f41705d4605d5ffaddada4ce04a3bc300571
Juan Tarca <jtarca@uade.edu.ar>
2025-11-23 23:28:20 -0300

    com

--- js/app.js
+++ js/app.js
@@ -293,4 +293,6 @@ document.addEventListener('DOMContentLoaded', () => {
   function escapeHtml(str) {
     return String(str).replace(/[&<>"']/g, s => ({'&':'&amp;','<':'&lt;','>':'&gt;','"':'&quot;',"'":'&#39;'}[s]));
   }
+
+  
 });
